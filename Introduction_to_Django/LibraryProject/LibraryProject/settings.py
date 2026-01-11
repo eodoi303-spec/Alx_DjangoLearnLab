8614717c6a1f60b8dfc8f96aa3f35d96d32880bf
Stedan Ampofo <stedanampofo19@gmail.com>
2026-01-11 15:10:29 +0000

    add all files

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -36,7 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',  
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bookshelf',   # 👈 add this
 ]
 
 MIDDLEWARE = [
